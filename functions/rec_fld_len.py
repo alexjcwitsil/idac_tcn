029b67cfa8e9e6f2c3b7812ffdb10e6bbdca59aa
alexjcwitsil <alexjcwitsil@gmail.com>
2021-07-07 12:30:34 -0400

    initilize idac tcn repo

---
+++ functions/rec_fld_len.py
@@ -0,0 +1,11 @@
+
+#  calculate the receptive field length of the neural network
+
+def rec_fld_len(pdict):
+    r = 0
+    for d in pdict['d']:
+        r = r + d * (pdict['k']-1)
+    return f'{r/pdict["r_smp"]} seconds'
+
+
+
